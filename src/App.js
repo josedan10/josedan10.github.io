eafc8a9f0df6f1111a583f6113bf701b34695220
José Daniel <josedanq100@gmail.com>
2019-04-21 22:18:11 -0400

    Router and sass urls

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import Index from './pages'
+import Portfolio from './pages/portfolio'
 class App extends Component {
   render() {
     return (
       <Router history={ createBrowserHistory() }>
         <div className="App">
-          <Route exact path='/' component={Index} />
+          <Switch>
+            <Route exact path='/' component={Index} />
+            <Route exatc path='/portfolio' component={Portfolio} />
+          </Switch>
         </div>
       </Router>
     );
