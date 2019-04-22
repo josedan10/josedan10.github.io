53283d9d8a389421833559d65c7b0606b486a6df
José Daniel <josedanq100@gmail.com>
2019-04-22 11:19:34 -0400

    Installed AOS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import AOS from 'aos'
+
 import Index from './pages'
 import Portfolio from './pages/portfolio'
 import NavBar from './components/NavBar'
+
+import 'aos/dist/aos.css'
+
+AOS.init()
+
 class App extends Component {
   render() {
     return (
