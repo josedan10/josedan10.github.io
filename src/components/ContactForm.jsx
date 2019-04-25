247b8b44682d005e1d74e6adb29f4ff7e069483c
Jose Daniel <josedanq100@gmail.com>
2019-04-25 14:35:36 -0400

    Setting up interaction in form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+export default (props) => {
+  let { handleChange, handleSubmit, completeds, errors,
+  status } = props
+
+  let totalCompleteds = Object.keys(completeds).filter(field => completeds[field]).length
+
+  return (
+    <form action='' className='console' onKeyDown={handleSubmit}>
+      <b className='blue'>sendMessage(</b>
+      <div className='input-group'>
+        &nbsp;&nbsp;
+        <label for='name' className='blue'>name: </label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <input id='name' name='name'
+          onChange={e => handleChange(e.target)} type='text'
+          placeholder='Jose' />
+      </div>
+
+      <div className='input-group'>
+        &nbsp;&nbsp;
+        <label for='lastname' className='blue'>lastname: </label>
+        <input id='lastname' name='lastname'
+          onChange={e => handleChange(e.target)} type='text'
+          placeholder='Quintero' />
+      </div>
+
+      <div className='input-group'>
+        &nbsp;&nbsp;
+        <label for='email' className='blue'>email: </label>
+        &nbsp;&nbsp;&nbsp;
+        <input id='email' name='email'
+          onChange={e => handleChange(e.target)} type='email'
+          placeholder='user@mail.com' />
+      </div>
+
+      <div className='input-group'>
+        &nbsp;&nbsp;
+        <label for='phone' className='blue'>phone: </label>
+        &nbsp;&nbsp;&nbsp;
+        <input id='phone' name='phone'
+          onChange={e => handleChange(e.target)} type='tel' pattern='[0-9]{1,3}-[0-9]{10}'
+          placeholder='58-4122353124' />
+      </div>
+
+      <div className='input-group'>
+        &nbsp;&nbsp;
+        <label for='subject' className='blue'>subject: </label>
+        &nbsp;
+        <input id='subject' name='subject'
+          onChange={e => handleChange(e.target)} type='text'
+          placeholder='Web responsive' />
+      </div>
+
+      <div className='input-group'>
+        &nbsp;&nbsp;
+        <label for='message' className='blue'>message: </label>
+        &nbsp;
+        <textarea id='message' name='message'
+          onChange={e => handleChange(e.target)} type='text'
+          placeholder='Tell me your idea...' />
+      </div>
+      <b className='blue'>)</b>
+      <br /><br />
+      <p className='blue'>
+        <b>Status:</b> { status }
+      </p>
+      <p className='blue'>
+        { totalCompleteds }/6 completed fields
+      </p>
+      { errors.length > 0 && errors.map(error => <p className='red'>{ error }</p>) }
+    </form>
+  )
+}
--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,7 +1,136 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
 
-export default (props) => {
-  return (
-    <h1>Contact</h1>
-  )
+export default class Contact extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: {
+        name: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        subject: '',
+        message: ''
+      },
+      status: 'In progress',
+      errors: [],
+      completeds: {}
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (target) {
+    this.setState(prevState => ({
+      ...prevState,
+      data: {
+        ...prevState.data,
+        [target.name]: target.value
+      },
+      completeds: {
+        ...prevState.completeds,
+        [target.name]: target.value.trim() !== ''
+      }
+    }))
+  }
+
+  isNotEmptyFields (fields) {
+    let isAnyEmpty = false
+    Object.keys(fields).map(field => {
+      if (fields[field].trim() === '') isAnyEmpty = true
+    })
+    return !isAnyEmpty
+  }
+
+  isValidFields (data) {
+    let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
+    let validPhone = /[0-9]{1,3}-[0-9]{10}/.test(data.phone)
+
+    return validEmail && validPhone
+  }
+
+  handleSubmit (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault()
+      
+      let Form = this
+      
+      if (!Form.isNotEmptyFields(Form.state.data)) {
+        Form.setState(prevState => ({
+          ...prevState,
+          errors: ['Some fields are empty. Please fill all fields'],
+          status: 'Error'
+        }))
+      }
+
+      if (!Form.isValidFields(Form.state.data)) {
+        Form.setState(prevState => ({
+          ...prevState,
+          errors: [...prevState.errors, 'Invalid format in email or phone'],
+          status: 'Error'
+        }))
+      }
+
+      if (!Form.isValidFields(Form.state.data) || !Form.isNotEmptyFields(Form.state.data)) {
+        setTimeout(() => {
+          Form.setState(prevState => ({
+            ...prevState,
+            errors: []
+          }))
+        }, 2000)
+        return
+      }
+
+      // Send Message
+      Form.setState(prevState => ({
+        ...prevState,
+        errors: [],
+        status: 'Sending message...'
+      }))
+    }
+  }
+
+  render () {
+    return (
+      <div id='Contact' className='container'>
+        <h2>Contact</h2>
+        <div className='content flex-center'>
+          <div className='schema'>
+            <pre>
+              <code>
+                <span className='blue'>function <span className='bold'>sendMessage </span>(</span>
+                <br />
+                &nbsp;&nbsp;{ '<name>,' } <br />
+                &nbsp;&nbsp;{ '<lastname>,' } <br />
+                &nbsp;&nbsp;{ '<email>,' } <br />
+                &nbsp;&nbsp;{ '<phone>,' } <br />
+                &nbsp;&nbsp;{ '<subject>,' } <br />
+                &nbsp;&nbsp;{ '<message>' } <br />
+                <span className='blue'>) {'{'} </span> <br />
+
+                &nbsp;&nbsp;<span className='blue'><span className='bold'>if </span>(</span> <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;{ 'name' } <span className='blue'>!==</span> { '"" &&' } <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;{ 'lastname' } <span className='blue'>!==</span> { '"" &&' } <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;{ 'email' } <span className='blue'>!==</span> { '"" &&' } <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;{ 'phone' } <span className='blue'>!==</span> { '"" &&' } <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;{ 'subject' } <span className='blue'>!==</span> { '"" &&' } <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;{ 'message' } <span className='blue'>!==</span> { '""' } <br />
+                &nbsp;&nbsp;<span className='blue'>) <span className='bold'>console.log</span>("Sent message") </span> <br />
+                &nbsp;&nbsp;<span className='blue'><span className='bold'>else </span><span className='bold'>console.log</span>("Error sending message")</span> <br />
+                <span className='blue'>}</span>
+              </code>
+            </pre>
+          </div>
+          <ContactForm
+            completeds={this.state.completeds}
+            errors={this.state.errors}
+            status={this.state.status}
+            handleSubmit={this.handleSubmit}
+            handleChange={this.handleChange} />
+        </div>
+      </div>
+    )
+  }
 }
