030a813347f7f76690e09fac116c19a1f959b3fa
José Daniel <josedanq100@gmail.com>
2019-04-27 11:10:59 -0400

    Updates

--- src/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const {
-  APP_ENV
-} = process.env
-
-// export const baseUrl = (APP_ENV === 'production') ? 'https://josedan10.github.io' : 'http://localhost:3000'
-export const baseUrl = 'https://josedan10.github.io'
-export const EmailJSUserID = 'user_LEWwWousyJSr9c2tm9kzM'
